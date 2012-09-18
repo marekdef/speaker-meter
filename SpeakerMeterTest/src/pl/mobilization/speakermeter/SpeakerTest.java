db84168b4afafd034220a07099b5950111411c7a
mariopce <mariopce@gmail.com>
2012-09-18 21:24:09 +0200

    add test project

---
+++ SpeakerMeterTest/src/pl/mobilization/speakermeter/SpeakerTest.java
@@ -0,0 +1,16 @@
+package pl.mobilization.speakermeter;
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+
+public class SpeakerTest extends AndroidTestCase {
+
+	
+	
+	public void testExample(){
+		//GIVEN We have presentation 
+		//WHEN today
+		//THEN present
+		Assert.assertTrue("Shoule be true", true);
+	}
+}
